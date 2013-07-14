90a3d5f1128f88376b6cab533a0cb501134856d2
Adam Kehoe <kehoe.adam@gmail.com>
2013-07-14 13:07:40 -0500

    read from index.html and return as response

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  var text = fs.readFileSync('index.html');
+  var response_string = text.toString('utf-8');
+  response.send(response_string);
 });
 
 var port = process.env.PORT || 5000;
